bf443615a3c9df1f953799de22a965daa255e11f
michaellee8 <ckmichael8@gmail.com>
2022-06-14 21:19:14 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DynProxy
+
+DynProxy is a raw TCP proxy that provides dynamically change the configuration of the proxy while keeping connections that should be unaffected.
---
+++ cmd/tcpecho/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	goerrors "errors"
+	"flag"
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var port int
+	if envPortStr, ok := os.LookupEnv("PORT"); ok {
+		envPort, err := strconv.Atoi(envPortStr)
+		if err != nil {
+			logrus.Fatal(errors.Wrap(err, "unable to parse port from command line"))
+			return
+		}
+		port = envPort
+	} else {
+		flag.IntVar(&port, "port", 10000, "port to listen on")
+	}
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		logrus.Fatal(errors.Wrap(err, "unable to listen for tcp"))
+	}
+	defer func() {
+		if err := lis.Close(); err != nil {
+			logrus.Error(errors.Wrap(err, "unable to close server"))
+		}
+	}()
+	logrus.Info("proxy started")
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			logrus.Error("cannot accept tcp connection")
+			break
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	if _, err := io.Copy(conn, conn); err != nil {
+		if !goerrors.Is(err, net.ErrClosed) {
+			logrus.Error(errors.Wrap(err, "unable to copy conn"))
+		}
+		_ = conn.Close()
+		return
+	}
+}
