3013af50257217be97a7371ff26d997e3d0a5482
michaellee8 <ckmichael8@gmail.com>
2022-07-03 21:31:14 +0800

    chore: wip

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -1,6 +1,30 @@
 package proxy
 
-type appTargetTuple struct {
-	app    string
+import (
+	"github.com/michaellee8/dynproxy/pkg/ds/targetset"
+	"net"
+	"sync"
+)
+import gsync "github.com/SaveTheRbtz/generic-sync-map-go"
+
+type ruleTargetMapKey struct {
+	rule   string
 	target string
+}
+
+type portMapValue struct {
+	rule    string
+	lis     *net.Listener
+	connSet *gsync.MapOf[*net.TCPConn, struct{}]
+	mut     *sync.RWMutex
+}
+
+type ruleMapValue struct {
+	targetSet *TargetSetWithHealthCheck
+	picker    *targetset.Picker[string]
+}
+
+type ruleTargetMapValue struct {
+	connSet *gsync.MapOf[*net.TCPConn, struct{}]
+	mut     *sync.RWMutex
 }
