aff229a33c25bd2a91773cce8f71ad6fa8bc0b16
michaellee8 <ckmichael8@gmail.com>
2022-07-07 11:01:23 +0800

    chore: wip

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -7,6 +7,9 @@ import (
 )
 import gsync "github.com/SaveTheRbtz/generic-sync-map-go"
 
+// Good reference for connection close handling:
+// https://github.com/mholt/caddy-l4/blob/56bd7700d889f2ffd52353c241819ddbc7745ff6/modules/l4proxy/proxy.go#L281
+
 type ruleTargetMapKey struct {
 	rule   string
 	target string
